3366f356eddb375d3aab54875f07250ed3e583f2
Vanessa <vanessa.lynne.blue@gmail.com>
2017-06-09 19:41:11 -0400

    more html / formatting added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -18,18 +18,24 @@ function getCrystalNumber() {
   return Math.floor( Math.random() * (12 - 1) + 1 );
 } 
 
-window.onload = function(event) {
-  randomComputerNum = computerRandomNum();
-  randomCrystalNum1 = getCrystalNumber();
-  randomCrystalNum2 = getCrystalNumber();
-  randomCrystalNum3 = getCrystalNumber();
-  randomCrystalNum4 = getCrystalNumber();
-  console.log("In onload event: " + randomComputerNum);
-  console.log("In onload event crystal 1: " + randomCrystalNum1);
-  console.log("In onload event crystal 2: " + randomCrystalNum2);
-  console.log("In onload event crystal 3: " + randomCrystalNum3);
-  console.log("In onload event crystal 4: " + randomCrystalNum4);
-}
+$(document).ready(function() {
+ 'use strict';
+   alert('inside of ready');
+ });
+
+
+// window.onload = function(event) {
+//   randomComputerNum = computerRandomNum();
+//   randomCrystalNum1 = getCrystalNumber();
+//   randomCrystalNum2 = getCrystalNumber();
+//   randomCrystalNum3 = getCrystalNumber();
+//   randomCrystalNum4 = getCrystalNumber();
+//   console.log("In onload event: " + randomComputerNum);
+//   console.log("In onload event crystal 1: " + randomCrystalNum1);
+//   console.log("In onload event crystal 2: " + randomCrystalNum2);
+//   console.log("In onload event crystal 3: " + randomCrystalNum3);
+//   console.log("In onload event crystal 4: " + randomCrystalNum4);
+// }
 
 
 
