26f09ec67eac91b4e6617aeda9da9db1273a25bf
Vanessa <vanessa.lynne.blue@gmail.com>
2017-06-10 14:23:42 -0400

    1st working product

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -18,10 +18,74 @@ function getCrystalNumber() {
   return Math.floor( Math.random() * (12 - 1) + 1 );
 } 
 
+function updateWins() {
+  wins++;
+  $('#wins').html(wins);
+}
+
+function updateLoses() {
+  loses++;
+  $('#loses').html(loses);
+}
+
+function updateScore() {
+  $('#score').html(userTotalScoreCount);
+}
+
+function resetGame () {
+
+  randomComputerNum = computerRandomNum();
+  $('#computerscore').html(randomComputerNum);
+  randomCrystalNum1 = getCrystalNumber();
+  $('#crystal1').attr('data-value', randomCrystalNum1);
+  randomCrystalNum2 = getCrystalNumber();
+  $('#crystal2').attr('data-value', randomCrystalNum2);
+  randomCrystalNum3 = getCrystalNumber();
+  $('#crystal3').attr('data-value', randomCrystalNum3);
+  randomCrystalNum4 = getCrystalNumber();
+  $('#crystal4').attr('data-value', randomCrystalNum4);
+
+  userTotalScoreCount = 0;
+  updateScore();
+
+}
+
+function updateGameOver(text) {
+    $('#gameover').html(text);
+}
+
 $(document).ready(function() {
- 'use strict';
-  //alert('inside of ready');
- });
+ 
+  resetGame();
+
+  
+  $(".crystal").on('click', function(event) {
+    // alert(event); 
+    userTotalScoreCount += parseInt($(this).attr('data-value'));
+    updateScore();
+
+    if (randomComputerNum === userTotalScoreCount) {
+        updateWins();
+        updateGameOver('You won...let\'s play again!');
+        resetGame();
+
+
+    } else if (randomComputerNum < userTotalScoreCount) {
+         updateLoses();
+         updateGameOver('You lost...let\'s play again!') ;      
+         resetGame();
+    }
+
+
+
+
+   })
+
+
+
+
+
+ })
 
 
 // window.onload = function(event) {
@@ -30,7 +94,7 @@ $(document).ready(function() {
 //   randomCrystalNum2 = getCrystalNumber();
 //   randomCrystalNum3 = getCrystalNumber();
 //   randomCrystalNum4 = getCrystalNumber();
-//   console.log("In onload event: " + randomComputerNum);
+//   console.log("In onload event: " + randomComputerNum);"
 //   console.log("In onload event crystal 1: " + randomCrystalNum1);
 //   console.log("In onload event crystal 2: " + randomCrystalNum2);
 //   console.log("In onload event crystal 3: " + randomCrystalNum3);
